9976f972877b9afabea44971dd7229441fa844dc
kenpeter <figo2476@hotmail.com>
2017-03-19 23:40:17 +1100

    update

--- osmosis_seek.js
+++ osmosis_seek.js
@@ -5,37 +5,45 @@ var JobDAO = require('./model/job');
 var job = new JobDAO();
 var searchUrl = 'https://www.seek.com.au/php-jobs/in-melbourne';
 
-/**** Same as above ****/
-osmosis
-  .get(searchUrl)
-  .find('article')
-  .set({
-    'script': 'script'
-  })
-  .data((data) => {
-    console.log('--- single data obj ---');
-    let script = data.script;
-    script = JSON.parse(script);
+var osmosisPromise = new Promise((resolve, reject) => {
+  osmosis
+    .get(searchUrl)
+    .find('article')
+    .set({
+      'script': 'script'
+    })
+    .data((data) => {
+      console.log('--- single data obj ---');
+      let script = data.script;
+      script = JSON.parse(script);
 
-    let datePosted = script.datePosted;
-    let description = script.description;
-    let employmentType = script.employmentType;
-    let orgName = script.hiringOrganization.name;
+      let datePosted = script.datePosted;
+      let description = script.description;
+      let employmentType = script.employmentType;
+      let orgName = script.hiringOrganization.name;
 
-    let addressLocal = script.jobLocation.address.addressLocality;
-    let addressRegion = script.jobLocation.address.addressRegion;
-    let jobTitle = script.title;
-    let jobUrl = script.url;
+      let addressLocal = script.jobLocation.address.addressLocality;
+      let addressRegion = script.jobLocation.address.addressRegion;
+      let jobTitle = script.title;
+      let jobUrl = script.url;
 
-    console.log(datePosted);
-    console.log(description);
-    console.log(employmentType);
-    console.log(orgName);
+      console.log(datePosted);
+      console.log(description);
+      console.log(employmentType);
+      console.log(orgName);
 
-    console.log(addressLocal);
-    console.log(addressRegion);
-    console.log(jobTitle);
-    console.log(jobUrl);
-  })
-  .error(console.log);
-  //.debug(console.log);
+      console.log(addressLocal);
+      console.log(addressRegion);
+      console.log(jobTitle);
+      console.log(jobUrl);
+
+      resolve();
+    })
+    .error(console.log);
+    //.debug(console.log);
+});
+
+osmosisPromise.then(() => {
+  console.log('--- all done ---');
+  process.exit(0);
+});
