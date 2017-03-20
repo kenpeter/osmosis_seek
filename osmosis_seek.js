13e1602a2874bafef662263c3be893db26ef598c
kenpeter <figo2476@hotmail.com>
2017-03-20 17:01:35 +1100

    update

--- osmosis_seek.js
+++ osmosis_seek.js
@@ -2,61 +2,27 @@ var osmosis = require('osmosis');
 var Promise = require('bluebird');
 var JobDAO = require('./model/job');
 
+const jobDAO = new JobDAO();
+
 const theListArr = [
-  'https://www.seek.com.au/php-jobs/in-All-Melbourne-VIC?page=',
-  'https://www.seek.com.au/nodejs-jobs/in-All-Melbourne-VIC?page='
+  {
+    list: 'https://www.seek.com.au/nodejs-jobs/in-All-Melbourne-VIC?page=',
+    num: 4
+  },
+  {
+    list: 'https://www.seek.com.au/javascript-jobs/in-All-Melbourne-VIC?page=',
+    num: 10
+  },
+  {
+    list: 'https://www.seek.com.au/php-jobs/in-All-Melbourne-VIC?page=',
+    num: 10 // max 10
+  },
+  {
+    list: 'https://www.seek.com.au/IT-jobs/in-All-Melbourne-VIC?page=',
+    num: 10 // max 10
+  }
 ];
-const theList = 'https://www.seek.com.au/nodejs-jobs/in-All-Melbourne-VIC?page=';
-const theListLength = 10;
 
-//var job = new JobDAO();
-
-/*
-var osmosisPromise = new Promise((resolve, reject) => {
-  osmosis
-    .get(searchUrl)
-    .find('article')
-    .set({
-      'script': 'script'
-    })
-    .data((data) => {
-      console.log('--- single data obj ---');
-      let script = data.script;
-      script = JSON.parse(script);
-
-      let datePosted = script.datePosted;
-      let description = script.description;
-      let employmentType = script.employmentType;
-      let orgName = script.hiringOrganization.name;
-
-      let addressLocal = script.jobLocation.address.addressLocality;
-      let addressRegion = script.jobLocation.address.addressRegion;
-      let jobTitle = script.title;
-      let jobUrl = script.url;
-
-      console.log(datePosted);
-      console.log(description);
-      console.log(employmentType);
-      console.log(orgName);
-
-      console.log(addressLocal);
-      console.log(addressRegion);
-      console.log(jobTitle);
-      console.log(jobUrl);
-
-      resolve();
-    })
-    .error(console.log);
-    //.debug(console.log);
-});
-*/
-
-/*
-osmosisPromise.then(() => {
-  console.log('--- all done ---');
-  process.exit(0);
-});
-*/
 
 function genEachPage(theList, theListLength) {
   const theReturn = [];
@@ -69,64 +35,91 @@ function genEachPage(theList, theListLength) {
   return theReturn;
 }
 
+function cleanup() {
+  return jobDAO.delete();
+}
 
-function main() {
-  let jobArr = genEachPage(theList, theListLength);
-  //console.log(jobArr);
-
-  return Promise.each(jobArr, (singlePage) => {
+function run() {
+  return Promise.each(theListArr, (listObj) => {
     return new Promise((resolve, reject) => {
-      console.log('~~~~~~' + singlePage + '~~~~~~');
+      console.log('====' + listObj.list + '====');
+      let jobArr = genEachPage(listObj.list, listObj.num);
 
-      osmosis
-        .get(singlePage)
-        .find('article')
-        .set({
-          'script': 'script'
-        })
-        .data((data) => {
-          console.log('-------- single data obj -------');
-          let script = data.script;
-          script = JSON.parse(script);
+      Promise.each(jobArr, (singlePage) => {
+        return new Promise((resolve1, reject1) => {
+          console.log();
+          console.log('~~~~~~' + singlePage + '~~~~~~');
 
-          let datePosted = script.datePosted;
-          let description = script.description;
-          let employmentType = script.employmentType;
-          let orgName = script.hiringOrganization.name;
+          osmosis
+            .get(singlePage)
+            .find('article')
+            .set({
+              'script': 'script'
+            })
+            .data((data) => {
+              console.log();
+              console.log('-------- single data obj -------');
 
-          let addressLocal = script.jobLocation.address.addressLocality;
-          let addressRegion = script.jobLocation.address.addressRegion;
-          let jobTitle = script.title;
-          let jobUrl = script.url;
+              let script = data.script;
+              script = JSON.parse(script);
 
-          console.log(datePosted);
-          console.log(description);
-          console.log(employmentType);
-          console.log(orgName);
+              let datePosted = script.datePosted;
+              let description = script.description;
+              let employmentType = script.employmentType;
+              let orgName = script.hiringOrganization.name;
 
-          console.log(addressLocal);
-          console.log(addressRegion);
-          console.log(jobTitle);
-          console.log(jobUrl);
+              let addressLocal = script.jobLocation.address.addressLocality;
+              let addressRegion = script.jobLocation.address.addressRegion;
+              let jobTitle = script.title;
+              let jobUrl = script.url;
 
-          resolve();
-        })
-        .error((err) => {
-          console.error(err);
-          reject();
+              console.log(datePosted);
+              console.log(description);
+              console.log(employmentType);
+              console.log(orgName);
+
+              console.log(addressLocal);
+              console.log(addressRegion);
+              console.log(jobTitle);
+              console.log(jobUrl);
+
+              resolve1();
+            })
+            .error((err) => {
+              console.log('--- single page error:' + singlePage);
+              console.error(err);
+              reject1();
+            });
+
         });
+      })
+      .then(() => {
+        // ..............
+        resolve();
+      })
+      .catch((err) => {
+        console.log('--- page array error:' + theList);
+        console.error(err);
+        reject();
+      });
+
     });
+
   });
 
 }
 
 
-main()
+cleanup()
+.then(() => {
+  return run();
+})
 .then(() => {
   console.log('--- all done ---');
   process.exit(0);
 })
 .catch(err => {
+  console.log('--- main error ---');
   console.error(err);
   process.exit(1);
 });
